db1a5c62fab187a0c21f4458f8f2b19e8591e025
hondamach <136286740+hondamach@users.noreply.github.com>
2024-03-31 20:14:38 +0700

    begin MVC

---
+++ app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const mainRoutes = require('./routes/mainpage')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'css')));
+
+app.use(mainRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
+
+app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path')
+
+const router = express.Router();
+
+module.exports = router;
