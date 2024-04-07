dbfd697be888011b84bff1b255fd020faed991f7
hondamach <136286740+hondamach@users.noreply.github.com>
2024-04-07 15:20:02 +0700

    routing mainpage

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const mainRoutes = require('./routes/mainpage')
-
+const mainRoutes = require('./routes/mainpage');
+const adminRoutes = require('./routes/admin');
 app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'css')));
+
+/*app.use((req,res,next)=> {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Tyoe,Authorization');
+    next();
+});*/
 
 app.use(mainRoutes);
+
+//app.use('/admin',adminRoutes);
 
 app.use((req,res,next) => {
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
-app.listen(3000);
+app.listen(8080);
--- routes/mainpage.js
+++ routes/mainpage.js
@@ -9,5 +9,23 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'index.html'));
   });
-  
+
+router.get('/dichvuindex.html', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'dichvuindex.html'));
+  });
+router.get('/tranglienhe+dkthongtin.html', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'tranglienhe+dkthongtin.html'));
+  });
+router.get('/trangthai.html', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'trangthai.html'));
+  });
+router.get('/hoso.html', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'hoso.html'));
+  });
+router.get('/datlich.html', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'datlich.html'));
+  });
+router.get('/dangnhap.html', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'dangnhap.html'));
+  });
 module.exports = router;
