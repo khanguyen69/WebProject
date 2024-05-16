0f2fa2b45980fe6741001f396554f72774de7cb3
kiet-uit-k17 <145459744+kiet-uit-k17@users.noreply.github.com>
2024-05-16 21:35:35 +0700

    Update connect.php
    thêm ghi chú

--- Backend/connect.php
+++ Backend/connect.php
@@ -1,3 +1,4 @@
-<?php
-    $conn = mysqli_connect('localhost','root','','petshop1');
+<?php 
+// nếu gặp lỗi khi kết nối cở sơ dữ liệu hãy check đúng tên database 
+    $conn = mysqli_connect('localhost','root','','petshop1'); // Name of DataBase 
 ?>
