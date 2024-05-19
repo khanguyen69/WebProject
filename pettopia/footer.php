df05d76a1bd2c844bd8041f3233d97af9bb7da93
khanguyen69 <117447960+khanguyen69@users.noreply.github.com>
2024-05-19 10:56:03 +0700

    footer_file

---
+++ pettopia/footer.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cuối trang</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        .footer {
+            width: 100%;
+            height: 250px;
+            padding: 10px;
+            box-sizing: border-box;
+            text-align: center;
+            background-color: #222222;
+            /* background-image: linear-gradient(#222222,#111111); */
+            color: #FFFFFF;
+        }
+        h1{
+            font-size: 20px;
+        }
+        .p {
+            font-size: 50px;
+            font-weight: bold;
+        }
+        .it{
+            font-size: 25px;
+            font-weight: bold;
+            color: #0000CC;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="trangcuoi">
+        <div class="footer">
+            <h1>Cung cấp dịch vụ chất lượng </h1>
+            <h1>và an toàn cho thú cưng của bạn</h1>
+            <p1>Điện thoại: (028) 372 51993, Ext: 112.</p1>
+            <br/>
+            <p1>Địa chỉ: TP Thủ Đức</p1>
+            <p><span class="p"><i><u>Petopia</u></i></span></p>
+        </div>
+    </div>
+</body>
+
+</html>
