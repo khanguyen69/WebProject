1fc2ab81072e07415b4ac095f7325e03964dddf1
kiet-uit-k17 <145459744+kiet-uit-k17@users.noreply.github.com>
2024-05-27 18:41:30 +0700

    Update dkuser.php
    chỉnh sửa nội dung

--- pettopia/dkuser.php
+++ pettopia/dkuser.php
@@ -77,8 +77,8 @@ if (
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng ký</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <style>
         body {
             background-image: linear-gradient(#f4d6cf, #8eccf5);
